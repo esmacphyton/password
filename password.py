09327d19fc621c1d4a359e9dcf84991c492f63e8
esmacphyton <161205169+esmacphyton@users.noreply.github.com>
2024-03-03 13:49:50 +0300

    Create password.py

---
+++ password.py
@@ -0,0 +1,17 @@
+import random
+karakterler = "+-/*!&$#?=abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+uzunluk = int(input("şifre uzunluğunu giriniz"))
+
+parola = ""
+for i in range(uzunluk):
+  parola +=
+
+random.choice(karakterler)
+
+print(parola)
+
+
+
+
+  
